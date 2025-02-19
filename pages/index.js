7eb058525e839f1805188f0d573c703207cb2095
MarcioBalbino <marciolado@gmail.com>
2025-02-19 12:00:33 +0000

    Alteração na frase principal.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Palloma, Deus te ama. Se você viu essa mensagem de uma risada!</h1>
+    return <h1>Piettra, Deus te ama e eu também. Se você viu essa mensagem de uma risada e me de um abraço!</h1>
 }
 
 export default Home;
