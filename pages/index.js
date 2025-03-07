e5cee6950d3478b4ac82e4d03be49932c97210d3
MarcioBalbino <marciolado@gmail.com>
2025-03-07 21:15:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Piettra, Deus te ama e eu também. Se você viu essa mensagem de uma risada e me de um abraço!</h1>
+  return (
+    <h1>
+      Piettra, Deus te ama e eu também. Se você viu essa mensagem de uma risada
+      e me de um abraço!
+    </h1>
+  );
 }
 
 export default Home;
